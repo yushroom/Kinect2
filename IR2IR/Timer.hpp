be41aac70172c76c56147c10bdd9f8498cabff7a
AKIKA <xiaokaneier@gmail.com>
2015-10-26 14:07:40 +0800

    update IR2IR

---
+++ IR2IR/Timer.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include "RenderFish.hpp"
+#include <chrono>
+
+class Timer
+{
+private:
+	typedef std::chrono::high_resolution_clock::time_point time_point;
+	//typedef std::chrono::time_point<std::chrono::steady_clock> time_point;
+	time_point _start;
+	time_point _end;
+	string _label;
+	bool _has_started = false;
+
+public:
+	Timer(string label) : _label(label) {}
+
+	void set_label(string label) {
+		_label = label;
+	}
+
+	void reset() {
+		_has_started = false;
+	}
+
+	void begin() {
+		_start = std::chrono::high_resolution_clock::now();
+		_has_started = true;
+	}
+	void end() {
+		if (!_has_started) {
+			error("timer %s not started.", _label);
+		}
+		_end = std::chrono::high_resolution_clock::now();
+		//_duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - _start).count();
+	}
+
+	void print() {
+		//info("Time info for [%s]: ", _label.c_str());
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - _start).count();
+		if (ms >= 1000)
+			info("Time info for [%s]: %f s\n", _label.c_str(), float(ms) / 1000);
+		else
+			info("Time info for [%s]: %lld ms\n", _label.c_str(), ms);
+	}
+	//friend std::ostream& operator<<(std::ostream& os, const Timer& timer) {
+	//	log("Time info for [%s]:\n", timer.label);
+	//	os << "Time info for [" << label << "]:"
+	//}
+};
+
--- IR2IR/main.cpp
+++ IR2IR/main.cpp
@@ -3,20 +3,35 @@
 #include <atlimage.h>
 #include <codex_math.h>
 
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 //#include "opencv2/contrib/contrib.hpp"
+
+#include "Timer.hpp"
+
 using namespace cv;
 using namespace std;
 
 typedef unsigned short UINT16;
 
+#ifdef _DEBUG
+#define WRITE_FILE
+#endif
+
+#define WRITE_FILE
+
+#ifdef WRITE_FILE
+static const bool write_file = true;
+#else
+static const bool write_file = false;
+#endif
+
 #define WIDTH 640
 #define HEIGHT 480
 
 #define ROOT_DIR			"D:\\project\\"
-#define NUMBER_ID			"04-31-42-0"
+#define NUMBER_ID			"02-43-09-0"
 #define IMAGE_DIR			ROOT_DIR NUMBER_ID "\\"
 #define PREFIX_A			IMAGE_DIR "a-" NUMBER_ID
 #define PREFIX_B			IMAGE_DIR "b-" NUMBER_ID
@@ -95,14 +110,6 @@ Mat ToCVImage(const vector<UINT16>& src)
 
 inline void writePly(const Mat& points, const Mat& IR, const char *filename)
 {
-	//int count = 0;
-	//for (int i = 0; i < WIDTH * HEIGHT; ++i) {
-	//	float x = points.at<float>(0, i);
-	//	float y = points.at<float>(1, i);
-	//	float z = points.at<float>(2, i);
-	//	if (!ZERO(x*y*z))
-	//		count++;
-	//}
 	FILE *fp;
 	fopen_s(&fp, filename, "w");
 	fprintf(fp, "ply\n");
@@ -126,13 +133,8 @@ inline void writePly(const Mat& points, const Mat& IR, const char *filename)
 			float x = points.at<float>(0, idx);
 			float y = points.at<float>(1, idx);
 			float z = points.at<float>(2, idx);
-			//if (ZERO(x*y*z))
-			//	continue;
 			fprintf_s(fp, "%f %f %f %d %d %d\n", x, y, z, c, c, c);
 		}
-	//for (int i = 0; i < WIDTH * HEIGHT; i++) {
-	//	fprintf_s(fp, "%f %f %f %d %d %d\n", point_cloud[i].x, point_cloud[i].y, point_cloud[i].z, infrared_pixels[i], infrared_pixels[i], infrared_pixels[i]);
-	//}
 	fclose(fp);
 }
 
@@ -144,7 +146,7 @@ inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, co
 	fprintf(fp, "ply\n");
 	fprintf(fp, "format ascii 1.0\n");
 	fprintf(fp, "comment file created by Microsoft Kinect Fusion\n");
-	fprintf(fp, "element vertex %d\n", (WIDTH - offset*2) * (HEIGHT - offset*2) * 2);
+	fprintf(fp, "element vertex %d\n", (WIDTH - offset * 2) * (HEIGHT - offset * 2) * 2);
 	fprintf(fp, "property float x\n");
 	fprintf(fp, "property float y\n");
 	fprintf(fp, "property float z\n");
@@ -156,8 +158,8 @@ inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, co
 	fprintf(fp, "end_header\n");
 
 
-	for (int j = offset; j < HEIGHT-offset; ++j)
-		for (int i = offset; i < WIDTH-offset; ++i) {
+	for (int j = offset; j < HEIGHT - offset; ++j)
+		for (int i = offset; i < WIDTH - offset; ++i) {
 			int idx = j * WIDTH + i;
 			int c = IR1.at<unsigned char>(j, i);
 			float x = points1.at<float>(0, idx);
@@ -167,8 +169,8 @@ inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, co
 			//	continue;
 			fprintf_s(fp, "%f %f %f %d %d %d\n", x, y, z, c, c, 0);	// v1 Yellow
 		}
-	for (int j = offset; j < HEIGHT-offset; ++j)
-		for (int i = offset; i < WIDTH-offset; ++i) {
+	for (int j = offset; j < HEIGHT - offset; ++j)
+		for (int i = offset; i < WIDTH - offset; ++i) {
 			int idx = j * WIDTH + i;
 			int c = IR2.at<unsigned char>(j, i);
 			float x = points2.at<float>(0, idx);
@@ -186,33 +188,89 @@ inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, co
 
 
 Mat calc_points(Mat depth_image, float fx, float fy, float cx, float cy) {
+	float inv_fx = 1.f / fx;
+	float inv_fy = 1.f / fy;
+	//float x, y, z;
 	Mat points(3, WIDTH * HEIGHT, CV_32F);
 	for (int j = 0; j < HEIGHT; ++j)
 		for (int i = 0; i < WIDTH; ++i) {
 			int idx = j * WIDTH + i;
 			UINT16 d = depth_image.at<UINT16>(j, i);
-			//if (d == 0)
-			//	cout << "0";
-			float x = (i - cx) / fx * d;
-			float y = (j - cy) / fy * d;
-			float z = d;
-			points.at<float>(0, idx) = (i + 0.5f - cx) / fx * d; // x
-			points.at<float>(1, idx) = (j + 0.5f - cy) / fy * d;
+			//float x_scale = inv_fx * d;
+			//float y_scale = inv_fy * d;
+			points.at<float>(0, idx) = (i + 0.5f - cx) * inv_fx * d; // x
+			points.at<float>(1, idx) = (j + 0.5f - cy) * inv_fy * d;
 			points.at<float>(2, idx) = d;
-			//points.at<float>(3, idx) = 1.f;
-			//cout << x << ' ' << y << ' ' << z << '\n';
 		}
 
 	return points;
 }
 
+void flood_fill(Mat& img, int offset = 2)
+{
+	Mat temp = img.clone();
+	for (int j = 0; j < HEIGHT; ++j) {
+		int i = 0;
+		while (i < WIDTH && img.at<UINT16>(j, i) == 0)
+			++i;
+		//cout << i << endl;
+		for (int k = 0; k < offset && i + k < WIDTH; ++k)
+			temp.at<UINT16>(j, i + k) = 0;
+
+		i = WIDTH - 1;
+		while (i >= 0 && img.at<UINT16>(j, i) == 0)
+			--i;
+		for (int k = 0; k < offset && i - k >= 0; ++k)
+			temp.at<UINT16>(j, i - k) = 0;
+	}
+	for (int i = 0; i < WIDTH; ++i) {
+		int j = 0;
+		while (j < HEIGHT && img.at<UINT16>(j, i) == 0)
+			++j;
+		for (int k = 0; k < offset && j + k < HEIGHT; ++k)
+			temp.at<UINT16>(j + k, i) = 0;
+
+		j = HEIGHT - 1;
+		while (j >= 0 && img.at<UINT16>(j, i) == 0)
+			--j;
+		for (int k = 0; k < offset && j - k >= 0; ++k)
+			temp.at<UINT16>(j - k, i) = 0;
+	}
+
+
+	temp.copyTo(img);
+}
+
+void image_show(const char* title, const Mat& image) {
+	if (write_file)
+		imshow(title, image);
+}
+
+void image_write(const char* path, const Mat& image) {
+	if (write_file)
+		imwrite(path, image);
+}
+
 int main()
 {
+	Timer timer_all("All");
+	timer_all.begin();
+
+	Timer timer_prepare("All");
+	timer_prepare.begin();
+
+	Timer timer_mat("Mat");
+	timer_mat.begin();
 	Mat cameraMatrix[2], distCoeffs[2];
 	cameraMatrix[0] = Mat::eye(3, 3, CV_64F);
 	cameraMatrix[1] = Mat::eye(3, 3, CV_64F);
 	Mat R, T, E, F;
-	Mat R1, R2, P1, P2, Q;
+	//Mat R1, R2, P1, P2, Q;
+	timer_mat.end();
+	timer_mat.print();
+
+	Timer timer_read_file("Read file");
+	timer_read_file.begin();
 
 	{
 		const char * extrinsic_filename = EXTRINSICS_FILE_PATH;
@@ -237,13 +295,14 @@ int main()
 		//Mat R, T;
 		fs["R"] >> R;
 		fs["T"] >> T;
-		fs["R1"] >> R1;
-		fs["R2"] >> R2;
-		fs["P1"] >> P1;
-		fs["P2"] >> P2;
-		fs["Q"] >> Q;
-		printf("T = (%lf, %lf, %lf)\n", T.at<double>(0, 0), T.at<double>(1, 0), T.at<double>(2, 0));
+		//fs["R1"] >> R1;
+		//fs["R2"] >> R2;
+		//fs["P1"] >> P1;
+		//fs["P2"] >> P2;
+		//fs["Q"] >> Q;
+		//printf("T = (%lf, %lf, %lf)\n", T.at<double>(0, 0), T.at<double>(1, 0), T.at<double>(2, 0));
 	}
+
 
 	vector<UINT16>	depth_v1_pixels;
 	vector<UINT16>	depth_v2_pixels;
@@ -272,17 +331,35 @@ int main()
 	img_IR[0] = imread(INFRARED_V1, 0);
 	img_IR[1] = imread(INFRARED_V2, 0);
 
+	timer_read_file.end();
+	timer_read_file.print();
+
+	//Timer timer_init_undistort_map("init undistort map");
+	//timer_init_undistort_map.begin();
+	//Mat map11, map12, map21, map22;
+	//Size img_size(WIDTH, HEIGHT);
+	//// undistort only;
+	//initUndistortRectifyMap(cameraMatrix[0], distCoeffs[0], Mat::eye(3, 3, CV_64F), cameraMatrix[0], img_size, CV_16SC2, map11, map12);
+	//initUndistortRectifyMap(cameraMatrix[1], distCoeffs[1], Mat::eye(3, 3, CV_64F), cameraMatrix[1], img_size, CV_16SC2, map21, map22);
+	//timer_init_undistort_map.end();
+	//timer_init_undistort_map.print();
+	//Timer timer_test("Test");
+	//timer_test.begin();
+
+	//Timer timer_undistort("undistort");
+	//timer_undistort.begin();
+
 	// test undistort
 	//Mat img_IR_1_undistort = img_IR[0].clone();
 	Mat img_IR_1_undistort(HEIGHT, WIDTH, img_IR[0].type());
 	undistort(img_IR[0], img_IR_1_undistort, cameraMatrix[0], distCoeffs[0]);
-	//imshow("IR1 [undistort]", img_IR_1_undistort);
-	imwrite(UNDISTORT_IR1_PATH, img_IR_1_undistort);
+	
+	//image_write(UNDISTORT_IR1_PATH, img_IR_1_undistort);
 	//Mat img_IR_2_undistort = img_IR[1].clone();
 	Mat img_IR_2_undistort(HEIGHT, WIDTH, img_IR[1].type());
 	undistort(img_IR[1], img_IR_2_undistort, cameraMatrix[1], distCoeffs[1]);
-	//imshow("IR2 [undistort]", img_IR_2_undistort);
-	imwrite(UNDISTORT_IR2_PATH, img_IR_2_undistort);
+	
+	//image_write(UNDISTORT_IR2_PATH, img_IR_2_undistort);
 
 	//Mat img_depth_1_undistort = img_depth_1.clone();
 	Mat img_depth_1_undistort(HEIGHT, WIDTH, img_depth_1.type());
@@ -293,28 +370,51 @@ int main()
 	Mat img_depth_2_undistort(HEIGHT, WIDTH, img_depth_2.type());
 	undistort(img_depth_2, img_depth_2_undistort, cameraMatrix[1], distCoeffs[1]);
 
+
+	//remap(img_IR[0], img_IR_1_undistort, map11, map12, INTER_LINEAR);
+	//remap(img_depth_1, img_depth_1_undistort, map11, map12, INTER_LINEAR);
+	//remap(img_IR[1], img_IR_2_undistort, map21, map22, INTER_LINEAR);
+	//remap(img_depth_2, img_depth_2_undistort, map21, map22, INTER_LINEAR);
+	//Timer timer_flood_fill("Flood fill");
+	//timer_flood_fill.begin();
+	flood_fill(img_depth_1_undistort, 5);
+	flood_fill(img_depth_2_undistort, 10);
+	//timer_flood_fill.end();
+	//timer_flood_fill.print();
+	//timer_undistort.end();
+	//timer_undistort.print();
+
+	imshow("IR1 [undistort]", img_IR_1_undistort);
+	imshow("IR2 [undistort]", img_IR_2_undistort);
+
+
+#if 0
 	//Mat temp = img_depth_1_undistort.clone();
 	Mat temp(HEIGHT, WIDTH, img_depth_1_undistort.type());
 	for (int j = 0; j < HEIGHT; ++j)
 		for (int i = 0; i < WIDTH; ++i)
 			temp.at<UINT16>(j, i) = int(img_depth_1_undistort.at<UINT16>(j, i)) % 256;
 	//cvtColor(temp, temp, CV_GRAY2RGB);
-	imwrite(UNDISTORT_DEPTH1_PATH, temp);
+	image_write(UNDISTORT_DEPTH1_PATH, temp);
 	for (int j = 0; j < HEIGHT; ++j)
 		for (int i = 0; i < WIDTH; ++i)
 			temp.at<UINT16>(j, i) = int(img_depth_2_undistort.at<UINT16>(j, i)) % 256;
-	imwrite(UNDISTORT_DEPTH2_PATH, temp);
+	image_write(UNDISTORT_DEPTH2_PATH, temp);
+#endif
 
-	imwrite("D:\\d.bmp", img_depth_2);
-	imwrite("D:\\d_u.bmp", img_depth_2_undistort);
-	writePly(calc_points(img_depth_2, V1_FX, V1_FY, V1_CX, V1_CY), img_IR[1], "D:\\point.ply");
-
+	//Timer timer_calc_points("Calc points");
+	//timer_calc_points.begin();
 	Mat points_1 = calc_points(img_depth_1_undistort, V1_FX, V1_FY, V1_CX, V1_CY);
 	Mat points_2 = calc_points(img_depth_2_undistort, V2_RESIZED_FX, V2_RESIZED_FY, V2_RESIZED_CX, V2_RESIZED_CY);
+	if (write_file) {
+		writePly(points_1, img_IR_1_undistort, POINTCLOUND_V1);
+		writePly(points_2, img_IR_2_undistort, POINTCLOUND_V2);
+	}
+	//timer_calc_points.end();
+	//timer_calc_points.print();
 
-	writePly(points_1, img_IR_1_undistort, POINTCLOUND_V1);
-	writePly(points_2, img_IR_2_undistort, POINTCLOUND_V2);
-
+#if 1
+	// 1 -> 2
 	{
 		Mat trans = Mat::zeros(3, 3, CV_32F);
 		for (int i = 0; i < 3; ++i)
@@ -354,16 +454,20 @@ int main()
 		}
 		//cout << "count = " << count << endl;
 		//imshow("RGB", img);
-		imwrite(V1_TO_V2_IMAGE, img);
+		image_write(V1_TO_V2_IMAGE, img);
 	}
-
+#endif
+#if 1
+	// 2 -> 1
+	//Timer timer_2to1("2->1");
+	//timer_2to1.begin();
 	{
 		Mat trans = Mat::zeros(3, 3, CV_32F);
 		for (int i = 0; i < 3; ++i)
 			for (int j = 0; j < 3; ++j)
 				trans.at<float>(i, j) = R.at<double>(j, i);
 		//for (int i = 0; i < 3; ++i)
-		//	trans.at<float>(i, 3) = T.at<double>(i);
+		//	trans.at<float>(i, 3) = -10*T.at<double>(i);
 
 		//points_1 = trans * points_1;
 		Mat points_2_2 = trans * points_2;
@@ -373,41 +477,47 @@ int main()
 		//tx = ty = tz = 0;
 		//cout << "tx = " << tx << "  ty = " << ty << "  tz = " << tz << endl;
 
+		Timer timer_write_ply("write ply");
+		timer_write_ply.begin();
 		writePly2(points_1, points_2_2, img_IR_1_undistort, img_IR_2_undistort, POINTCLOUND_ALL_1);
+		timer_write_ply.end();
+		timer_write_ply.print();
 
-		int count = 0;
-		//cout << img_IR[1].type() << endl;
 		Mat img = Mat::zeros(HEIGHT, WIDTH, img_IR[1].type());
-		//Mat img = img_IR_1_undistort.clone();
 		for (int j = 0; j < HEIGHT; ++j) {
 			for (int i = 0; i < WIDTH; ++i) {
 				int idx = j * WIDTH + i;
 				int x, y;
 				float z = points_2.at<float>(2, idx);
 				if (z <= 1e-2f && z >= -1e-2f) {
-					count++;
 					continue;
 				}
 				z = points_2_2.at<float>(2, idx) + tz;
 				x = int(V1_FX * (points_2_2.at<float>(0, idx) + tx) / z + V1_CX - 0.5f);
 				y = int(V1_FY * (points_2_2.at<float>(1, idx) + ty) / z + V1_CY - 0.5f);
+				//z = points_2_2.at<float>(2, idx);
+				//x = int(V1_FX * (points_2_2.at<float>(0, idx)) / z + V1_CX - 0.5f);
+				//y = int(V1_FY * (points_2_2.at<float>(1, idx)) / z + V1_CY - 0.5f);
 				//cout << x << ' ' << y << ' ' << z << '\n';
 				if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
 					img.at<unsigned char>(y, x) = img_IR_2_undistort.at<unsigned char>(j, i);
-					//cout << idx << ' ' << x << ' ' << y << ' ' << z << '\n';
-					//count++;
 				}
 			}
 		}
-		cout << "count = " << count << endl;
 		//imshow("RGB2", img);
-		imwrite(V2_TO_V1_IMAGE, img);
+		image_write(V2_TO_V1_IMAGE, img);
 	}
+	//timer_2to1.end();
+	//timer_2to1.print();
+	//timer_test.end();
+	//timer_test.print();
+#endif
 
-
-	//Mat img = img_IR_1_undistort.
-
-	//char c = (char)waitKey();
+	timer_all.end();
+	timer_all.print();
+	system("pause");
+	char c = (char)waitKey();
 	//if (c == 27 || c == 'q' || c == 'Q')
-		return 0;
+	//	return 0;
+	return 0;
 }
