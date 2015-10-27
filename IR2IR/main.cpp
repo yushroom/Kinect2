d2004946336bd6463a61e86b2916ede997578c31
AKIKA <xiaokaneier@gmail.com>
2015-10-27 09:58:30 +0800

    update

--- IR2IR/main.cpp
+++ IR2IR/main.cpp
@@ -8,7 +8,10 @@
 #include <opencv2/highgui/highgui.hpp>
 //#include "opencv2/contrib/contrib.hpp"
 
+#include <fstream>
+#include <algorithm>
 #include "Timer.hpp"
+#include <advmath.h>
 
 using namespace cv;
 using namespace std;
@@ -78,7 +81,7 @@ const int	depth_width = 640;
 const int	depth_height = 480;
 
 Mat ResizeAndToCVImage(const vector<UINT16> depth_pixels){
-	Mat mat(Size(512, 424), CV_16UC1);
+	Mat mat = Mat::zeros(Size(512, 424), CV_16UC1);
 	for (int j = 0; j < 424; j++) {
 		for (int i = 0; i < 512; ++i) {
 			mat.at<UINT16>(j, i) = depth_pixels[j * 512 + i];
@@ -87,7 +90,7 @@ Mat ResizeAndToCVImage(const vector<UINT16> depth_pixels){
 	//printf("%d %d\n", mat.cols, mat.rows);
 	cv::resize(mat, mat, Size(580, 480));
 	//printf("%d %d\n", mat.cols, mat.rows);
-	Mat new_mat(Size(640, 480), mat.type());
+	Mat new_mat = Mat::zeros(Size(640, 480), mat.type());
 	mat.copyTo(new_mat(Rect(30, 0, 580, 480)));
 	return new_mat;
 }
@@ -138,7 +141,47 @@ inline void writePly(const Mat& points, const Mat& IR, const char *filename)
 	fclose(fp);
 }
 
-inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, const Mat& IR2, const char *filename)
+struct Vertex {
+	float x, y, z;
+	unsigned char r, g, b;
+};
+
+//inline void writePly_binary(const Mat& points, const Mat& IR, const char *filename)
+//{
+//	ofstream fout(filename, ios::binary);
+//	fout << "ply\n";
+//	fout << "format binary_little_endian 1.0\n";
+//	fout << "element vertex " << WIDTH * HEIGHT << '\n';
+//	fout << "property float x\n"
+//		<< "property float y\n"
+//		<< "property float z\n"
+//		<< "property uchar red\n"
+//		<< "property uchar green\n"
+//		<< "property uchar blue\n"
+//		<< "element face 0\n"
+//		<< "property list uchar int vertex_index\n"
+//		<< "end_header\n";
+//
+//	vector<Vertex> vertices;
+//	vertices.reserve(WIDTH * HEIGHT);
+//	for (int j = 0; j < HEIGHT; ++j)
+//		for (int i = 0; i < WIDTH; ++i) {
+//			int idx = j * WIDTH + i;
+//			int c = IR.at<unsigned char>(j, i);
+//			float x = points.at<float>(0, idx);
+//			float y = points.at<float>(1, idx);
+//			float z = points.at<float>(2, idx);
+//			//fprintf_s(fp, "%f %f %f %d %d %d\n", x, y, z, c, c, c);
+//			//fout << x << y << z << c << c << c;
+//			if (z !=)
+//			vertices.push_back({ x, y, z, c, c, c });
+//		}
+//	fout.write((char*)&vertices[0], vertices.size() * sizeof(Vertex));
+//	fout.close();
+//}
+
+inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, const Mat& IR2, const char *filename,
+	float tx1 = 0, float ty1 = 0, float tz1 = 0, float tx2 = 0, float ty2 = 0, float tz2 = 0)
 {
 	int offset = 0;
 	FILE *fp;
@@ -162,9 +205,9 @@ inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, co
 		for (int i = offset; i < WIDTH - offset; ++i) {
 			int idx = j * WIDTH + i;
 			int c = IR1.at<unsigned char>(j, i);
-			float x = points1.at<float>(0, idx);
-			float y = points1.at<float>(1, idx);
-			float z = points1.at<float>(2, idx);
+			float x = points1.at<float>(0, idx) + tx1;
+			float y = points1.at<float>(1, idx) + ty1;
+			float z = points1.at<float>(2, idx) + tz1;
 			//if (x * y * z == 0)
 			//	continue;
 			fprintf_s(fp, "%f %f %f %d %d %d\n", x, y, z, c, c, 0);	// v1 Yellow
@@ -173,9 +216,9 @@ inline void writePly2(const Mat& points1, const Mat& points2, const Mat& IR1, co
 		for (int i = offset; i < WIDTH - offset; ++i) {
 			int idx = j * WIDTH + i;
 			int c = IR2.at<unsigned char>(j, i);
-			float x = points2.at<float>(0, idx);
-			float y = points2.at<float>(1, idx);
-			float z = points2.at<float>(2, idx);
+			float x = points2.at<float>(0, idx) + tx2;
+			float y = points2.at<float>(1, idx) + ty2;
+			float z = points2.at<float>(2, idx) + tz2;
 			//if (x * y * z == 0)
 			//	continue;
 			fprintf_s(fp, "%f %f %f %d %d %d\n", x, y, z, 0, 0, c);	// v2 blue
@@ -241,6 +284,14 @@ void flood_fill(Mat& img, int offset = 2)
 	temp.copyTo(img);
 }
 
+void get_valid_rect(const Mat& image, int* out_x, int* out_y, int* out_width, int* out_height)
+{
+	*out_x = 110;
+	*out_y = 60;
+	*out_width = 500 - 110;
+	*out_height = 360 - 60;
+}
+
 void image_show(const char* title, const Mat& image) {
 	if (write_file)
 		imshow(title, image);
@@ -249,6 +300,84 @@ void image_show(const char* title, const Mat& image) {
 void image_write(const char* path, const Mat& image) {
 	if (write_file)
 		imwrite(path, image);
+}
+
+void write_normalized_image(const Mat &img, const char *filename)
+{
+	int w = img.cols, h = img.rows;
+	double vmin, vmax;
+	minMaxLoc(img, &vmin, &vmax);
+
+	Mat output = Mat::zeros(h, w, CV_8U);
+	for (int y = 0; y < h; y++)
+		for (int x = 0; x < w; x++)
+		{
+			float v = (img.at<float>(y, x) - vmin) / (vmax - vmin);
+			output.at<BYTE>(y, x) = BYTE(v * 255);
+		}
+
+	image_write(filename, output);
+}
+
+
+void solve_for_bias(const int w, const int h, const std::vector<float> &I,
+	const float alpha, const float lambda, const int niter,
+	std::vector<float> &result)
+{
+	const int NUM_NEIGHBOR = 4;
+	static int dir[NUM_NEIGHBOR * 2] = { -1, 0, 1, 0, 0, -1, 0, 1 };
+
+	advmath::la_matrix_csr<float> A;
+	std::vector<float> vb;
+	A.column = w*h;
+
+	for (int y = 0; y < h; y++)
+		for (int x = 0; x < w; y++)
+		{
+			int idx_first, idx_last, idx = x + y*w;
+
+			//data term
+			idx_first = (int)A.val.size();
+			idx_last = (int)A.val.size();
+
+			A.val.push_back(alpha);
+			A.cols.push_back(idx);
+
+			A.ptrb.push_back(idx_first);
+			A.ptre.push_back(idx_last + 1);
+			vb.push_back(I[idx]);
+			A.row++;
+
+			//smoothness terms
+			for (int i = 0; i < NUM_NEIGHBOR; i++)
+			{
+				int xx = x + dir[i * 2 + 0],
+					yy = y + dir[i * 2 + 1];
+				if (xx >= 0 && xx < w && yy >= 0 && yy < h && idx < xx + yy*w)
+				{
+					idx_first = (int)A.val.size();
+					idx_last = (int)A.val.size();
+
+					A.val.push_back(1.0f);
+					A.cols.push_back(idx);
+					A.val.push_back(1.0f);
+					A.cols.push_back(xx + yy*w);
+
+					A.ptrb.push_back(idx_first);
+					A.ptre.push_back(idx_last + 1);
+					vb.push_back(0);
+					A.row++;
+				}
+			}
+		}
+
+	advmath::la_vector<float> b(vb);
+	//sparse least square
+	advmath::la_vector<float> x;
+	advmath::ssplsqr(x, A, b, lambda, niter, true);
+
+	result.resize(x.length);
+	memcpy(&result[0], x.v, sizeof(float)*x.length);
 }
 
 int main()
@@ -262,8 +391,8 @@ int main()
 	Timer timer_mat("Mat");
 	timer_mat.begin();
 	Mat cameraMatrix[2], distCoeffs[2];
-	cameraMatrix[0] = Mat::eye(3, 3, CV_64F);
-	cameraMatrix[1] = Mat::eye(3, 3, CV_64F);
+	//cameraMatrix[0] = Mat::eye(3, 3, CV_64F);
+	//cameraMatrix[1] = Mat::eye(3, 3, CV_64F);
 	Mat R, T, E, F;
 	//Mat R1, R2, P1, P2, Q;
 	timer_mat.end();
@@ -321,8 +450,12 @@ int main()
 		fclose(depth_in);
 	}
 
+	UINT max_ele = *std::max_element(depth_v2_pixels.begin(), depth_v2_pixels.end());
+
 	Mat img_depth_1 = ToCVImage(depth_v1_pixels);
 	Mat img_depth_2 = ResizeAndToCVImage(depth_v2_pixels);
+	double min, max;
+	minMaxLoc(img_depth_2, &min, &max);
 	//imshow("depth image 1", img_depth_1);
 	//imshow("depth image 2", img_depth_2);
 
@@ -334,17 +467,18 @@ int main()
 	timer_read_file.end();
 	timer_read_file.print();
 
-	//Timer timer_init_undistort_map("init undistort map");
-	//timer_init_undistort_map.begin();
-	//Mat map11, map12, map21, map22;
-	//Size img_size(WIDTH, HEIGHT);
-	//// undistort only;
-	//initUndistortRectifyMap(cameraMatrix[0], distCoeffs[0], Mat::eye(3, 3, CV_64F), cameraMatrix[0], img_size, CV_16SC2, map11, map12);
-	//initUndistortRectifyMap(cameraMatrix[1], distCoeffs[1], Mat::eye(3, 3, CV_64F), cameraMatrix[1], img_size, CV_16SC2, map21, map22);
-	//timer_init_undistort_map.end();
-	//timer_init_undistort_map.print();
-	//Timer timer_test("Test");
-	//timer_test.begin();
+	Timer timer_init_undistort_map("init undistort map");
+	timer_init_undistort_map.begin();
+	Mat map11, map12, map21, map22;
+	Size img_size(WIDTH, HEIGHT);
+	// undistort only;
+	initUndistortRectifyMap(cameraMatrix[0], distCoeffs[0], Mat::eye(3, 3, CV_64F), cameraMatrix[0], img_size, CV_16SC2, map11, map12);
+	initUndistortRectifyMap(cameraMatrix[1], distCoeffs[1], Mat::eye(3, 3, CV_64F), cameraMatrix[1], img_size, CV_16SC2, map21, map22);
+	timer_init_undistort_map.end();
+	timer_init_undistort_map.print();
+
+	Timer timer_test("Test");
+	timer_test.begin();
 
 	//Timer timer_undistort("undistort");
 	//timer_undistort.begin();
@@ -353,12 +487,12 @@ int main()
 	//Mat img_IR_1_undistort = img_IR[0].clone();
 	Mat img_IR_1_undistort(HEIGHT, WIDTH, img_IR[0].type());
 	undistort(img_IR[0], img_IR_1_undistort, cameraMatrix[0], distCoeffs[0]);
-	
+	//imshow("IR1 [undistort]", img_IR_1_undistort);
 	//image_write(UNDISTORT_IR1_PATH, img_IR_1_undistort);
 	//Mat img_IR_2_undistort = img_IR[1].clone();
 	Mat img_IR_2_undistort(HEIGHT, WIDTH, img_IR[1].type());
 	undistort(img_IR[1], img_IR_2_undistort, cameraMatrix[1], distCoeffs[1]);
-	
+	//imshow("IR2 [undistort]", img_IR_2_undistort);
 	//image_write(UNDISTORT_IR2_PATH, img_IR_2_undistort);
 
 	//Mat img_depth_1_undistort = img_depth_1.clone();
@@ -383,9 +517,6 @@ int main()
 	//timer_flood_fill.print();
 	//timer_undistort.end();
 	//timer_undistort.print();
-
-	imshow("IR1 [undistort]", img_IR_1_undistort);
-	imshow("IR2 [undistort]", img_IR_2_undistort);
 
 
 #if 0
@@ -431,33 +562,89 @@ int main()
 		//tx = ty = tz = 0;
 		//cout << "tx = " << tx << "  ty = " << ty << "  tz = " << tz << endl;
 
-		writePly2(points_1_1, points_2, img_IR_1_undistort, img_IR_2_undistort, POINTCLOUND_ALL_2);
+		writePly2(points_1_1, points_2, img_IR_1_undistort, img_IR_2_undistort, POINTCLOUND_ALL_2, tx, ty, tz, 0, 0, 0);
 
 		//int count = 0;
 		//cout << img_IR[1].type() << endl;
-		Mat img = Mat::zeros(HEIGHT, WIDTH, img_IR[1].type());
+		Mat img_new_IR = Mat::zeros(HEIGHT, WIDTH, img_IR[1].type());
+		Mat img_new_depth = Mat::zeros(HEIGHT, WIDTH, CV_32F);
 		//Mat img = img_IR_1_undistort.clone();
 		for (int j = 0; j < HEIGHT; ++j) {
 			for (int i = 0; i < WIDTH; ++i) {
 				int idx = j * WIDTH + i;
 				int x, y;
-				float z = points_1_1.at<float>(2, idx) + tz;
+				float z = points_1.at<float>(2, idx);
+				if (z <= 1e-2f && z >= -1e-2f) {
+					continue;
+				}
+				z = points_1_1.at<float>(2, idx) + tz;
 				x = int(V2_RESIZED_FX * (points_1_1.at<float>(0, idx) + tx) / z + V2_RESIZED_CX - 0.5f);
 				y = int(V2_RESIZED_FY * (points_1_1.at<float>(1, idx) + ty) / z + V2_RESIZED_CY - 0.5f);
 				//cout << x << ' ' << y << ' ' << z << '\n';
 				if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
-					img.at<unsigned char>(y, x) = img_IR_1_undistort.at<unsigned char>(j, i);
-					//cout << idx << ' ' << x << ' ' << y << ' ' << z << '\n';
-					//count++;
+					img_new_IR.at<unsigned char>(y, x) = img_IR_1_undistort.at<unsigned char>(j, i);
+					img_new_depth.at<float>(y, x) = z;
 				}
 			}
 		}
+		Mat img_depth_2_undistort_f = Mat::zeros(HEIGHT, WIDTH, CV_32F);
+		for (int j = 0; j < HEIGHT; ++j)
+			for (int i = 0; i < WIDTH; ++i) {
+				UINT16 val = img_depth_2_undistort.at<UINT16>(j, i);
+				if (val <  15000)
+					img_depth_2_undistort_f.at<float>(j, i) = (float)img_depth_2_undistort.at<UINT16>(j, i);
+			}
+
+		
+		//double min, max;
+		//minMaxLoc(img_depth_2_undistort_f, &min, &max);
+
+		Mat img_new_depth_cropped;
+		int width_crop = 0, height_crop = 0;
+		Rect roi;
+		{
+			int x = 0, y = 0;
+			get_valid_rect(img_new_depth, &x, &y, &width_crop, &height_crop);
+			roi = Rect(x, y, width_crop, height_crop);
+			img_new_depth(roi).copyTo(img_new_depth_cropped);
+		}
+		
+		write_normalized_image(img_new_depth_cropped, "D:\\depth_1_und.bmp");
+		write_normalized_image(img_depth_2_undistort_f(roi), "D:\\depth_2_und.bmp");
+		Mat img_diff = img_new_depth_cropped - img_depth_2_undistort_f(roi);
+		minMaxLoc(img_diff, &min, &max);
+		cout << "min and max: " << min << ' ' << max << endl;
+		Mat img_diff_g;
+		GaussianBlur(img_diff, img_diff_g, Size(0, 0), 4, 0);
+		img_diff = img_diff_g;
+		
+		Mat img_diff_pos = Mat::zeros(height_crop, width_crop, CV_8U);
+		Mat img_diff_neg = Mat::zeros(height_crop, width_crop, CV_8U);
+
+		//img_diff *= 1.f / max;
+		double inv_max = 255.f / std::max(abs(max), abs(min))  * 4.f;
+		//int count = 0;
+		for (int j = 0; j < height_crop; ++j)
+			for (int i = 0; i < width_crop; ++i) {
+				auto val = img_diff.at<float>(j, i);
+				if (val <= -0.1) {
+					img_diff_neg.at<unsigned char>(j, i) = unsigned char(-val * inv_max);
+				}
+				else {
+					img_diff_pos.at<unsigned char>(j, i) = unsigned char(val * inv_max);
+					//count++;
+				}
+			}
+		//cout << "count " << count << endl;
 		//cout << "count = " << count << endl;
 		//imshow("RGB", img);
-		image_write(V1_TO_V2_IMAGE, img);
+		imshow("cropped neg", img_diff_neg);
+		image_write(V1_TO_V2_IMAGE, img_new_IR);
+		image_write("D:\\depth_pos.bmp", img_diff_pos);
+		image_write("D:\\depth_neg.bmp", img_diff_neg);
 	}
 #endif
-#if 1
+#if 0
 	// 2 -> 1
 	//Timer timer_2to1("2->1");
 	//timer_2to1.begin();
@@ -509,13 +696,13 @@ int main()
 	}
 	//timer_2to1.end();
 	//timer_2to1.print();
-	//timer_test.end();
-	//timer_test.print();
+	timer_test.end();
+	timer_test.print();
 #endif
 
 	timer_all.end();
 	timer_all.print();
-	system("pause");
+	//system("pause");
 	char c = (char)waitKey();
 	//if (c == 27 || c == 'q' || c == 'Q')
 	//	return 0;
