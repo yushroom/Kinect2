33f264f8e2546e629b7f61116cc323238c674379
flexwang <zhaotianzju@gmail.com>
2015-11-24 20:41:39 +0800

    update depthFitLib

--- depthFitLib/dump_utils.h
+++ depthFitLib/dump_utils.h
@@ -1,8 +1,11 @@
 #pragma once
+#include "math_utils.h"
 
 #include <codex_basic.h>
 #include <codex_math.h>
-#include <vector>
+#include <vector>	
+
+#include <atlimage.h>
 
 typedef codex::math::vector::vector3<float>				vector3f;
 typedef codex::math::vector::vector3<int>				vector3i;
@@ -18,7 +21,18 @@ void dump_normalized_image(
 	const std::vector<T>&			image,
 	const char*						path,
 	const int						width,
-	const int						height);
+	const int						height)
+{
+	T lower_bound = *std::min_element(image.begin(), image.end());
+	T upper_bound = *std::max_element(image.begin(), image.end());
+	dump_normalized_image(
+		image,
+		path,
+		width,
+		height,
+		lower_bound,
+		upper_bound);
+}
 
 template <typename T>
 void dump_normalized_image(
@@ -27,5 +41,19 @@ void dump_normalized_image(
 	const int						width,
 	const int						height,
 	const T							lower_bound,
-	const T							upper_bound);
+	const T							upper_bound)
+{
+	const T range = upper_bound - lower_bound;
+	CImage cimage;
+	cimage.Create(width, height, 32);
+	for (int i = 0; i < height; ++i)
+		for (int j = 0; j < width; ++j) {
+		int idx = j + i*width;
+		T value = clamp(image[idx], lower_bound, upper_bound);
+		value = (value - lower_bound) / range;
+		BYTE rgb = static_cast<BYTE>(value * 255);
+		cimage.SetPixelRGB(j, i, rgb, rgb, rgb);
+		}
+	cimage.Save(path);
+}
 
